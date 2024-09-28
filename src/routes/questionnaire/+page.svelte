1cec841b004494aa6ff2729b5aacd44290225e64
prdcr-gem <deemaine628@gmail.com>
2024-09-28 17:01:45 -0400

    Created Form

---
+++ src/routes/questionnaire/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import type { PageData } from "./$types.js";
+    import Form from "$lib/components/Form.svelte";
+    export let data: PageData;
+</script>
+
+<Form data={data} />
