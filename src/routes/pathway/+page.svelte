5c63854b25724cd2ab2b4ab2ee0a9bb947595df3
“AlphauroraE” <brieoche99@gmail.com>
2024-09-28 17:16:31 -0400

    Try rounded rectangle

--- src/routes/pathway/+page.svelte
+++ src/routes/pathway/+page.svelte
@@ -1,13 +1,9 @@
 <script>
-    let inputValue = '';
-</script>
-
-<div class="flex justify-center items-center h-screen">
-<input
-    type="text"
-    bind:value={inputValue}
-    placeholder="Enter text here"
-    class="border border-gray-300 rounded-md p-2 w-1/3 focus:outline-none focus:ring-2 focus:ring-blue-500"
-/>
-</div>
+    // You can define any text or variables you want to display later
+    let text = "Your text here";
+  </script>
+  
+  <div class="bg-blue-500 rounded-lg p-4 shadow-md text-white">
+    <p>{text}</p>
+  </div>
   
