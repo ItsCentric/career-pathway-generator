849ac9d9f6b9685cba9040b9c7072ce3777b5d23
“AlphauroraE” <brieoche99@gmail.com>
2024-09-28 17:04:53 -0400

    Started working on output page

---
+++ src/routes/pathway/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let inputValue = '';
+</script>
+
+<div class="flex justify-center items-center h-screen">
+<input
+    type="text"
+    bind:value={inputValue}
+    placeholder="Enter text here"
+    class="border border-gray-300 rounded-md p-2 w-1/3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+/>
+</div>
+  
