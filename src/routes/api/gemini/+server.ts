6985ab480bfcd0749fd9dfd85df37e52085398d3
AlphauroraE <142271610+AlphauroraE@users.noreply.github.com>
2024-09-29 09:16:26 -0400

    Update +server.ts prompt

--- src/routes/api/gemini/+server.ts
+++ src/routes/api/gemini/+server.ts
@@ -8,7 +8,7 @@ export const GET: RequestHandler = async ({ url: reqUrl }) => {
 	const q3 = searchParams.get('q3');
 	const q4 = searchParams.get('q4');
 
-	const prompt = `I am a ${q3} level programmer who is familiar with ${q2} and is looking to learn ${q1}. I am willing to commit ${q4} hours a week to learning. Please generate a learning pathway with learning resources personal projects for a ${q3} level programmer. Please provide the output in JSON with the root key being schedule and then the following keys: week, topic, description, time_estimate, resources, resources.url, resources.title, resources.type, resources.description, projects, projects.title, projects.description. Please do not include markdown formatting`;
+	const prompt = `I want to learn ${q1}. I know ${q2}, and I am an ${q3}. I can commit ${q4} per week to learning this. Create an outline/schedule for me to learn ${q1}. Be detailed about time estimates as well as what resources (free and paid) are available to me. Also suggest projects I could do to learn ${q1} as an ${q3} level student. Output your response in JSON format - the output should just be JSON. The schedule should have the keys \"week\", \"topic\", \"description\", \"time_estimate\", \"resources\" for each resource which contains the following keys: \"title\", \"url\", \"type\", \"description\", and \"projects\" for each project which contains the following keys: \"title\", \"description\". Please do not include markdown formatting for the json.`;
 
 	const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
