de8e8a199e1be681abc016c36498ab6175cdd95f
Isaiah <isaiahmstockton@gmail.com>
2024-09-29 06:25:10 -0400

    hotfix: duplicate identifier

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -6,13 +6,12 @@ export const signUpFormSchema = z.object({
 });
 
 export type SignUpFormSchema = z.infer<typeof signUpFormSchema>;
-import { z } from "zod";
- 
+
 export const formSchema = z.object({
- question1: z.enum(['software engineering', 'web development', 'ai/ml']),
- question2: z.enum(['']),
- question3: z.enum(['Beginner, Intermediate', 'Advanced']),
- question4: z.enum(['1-2 hours, 2-5 hours, 5-8 hours'])
+	question1: z.enum(['software engineering', 'web development', 'ai/ml']),
+	question2: z.enum(['']),
+	question3: z.enum(['Beginner, Intermediate', 'Advanced']),
+	question4: z.enum(['1-2 hours, 2-5 hours, 5-8 hours'])
 });
 
 export type FormSchema = typeof formSchema;
